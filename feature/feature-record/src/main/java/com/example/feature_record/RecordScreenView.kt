83b908b06caf86669131c79c9d504f65745ded57
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-11-02 17:37:35 +0900

    feat: set background color for better appearance

--- feature/feature-record/src/main/java/com/example/feature_record/RecordScreenView.kt
+++ feature/feature-record/src/main/java/com/example/feature_record/RecordScreenView.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -54,7 +55,8 @@ fun RecordScreenView(
         },
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
-        }
+        },
+        containerColor = MaterialTheme.colorScheme.surface,
     ) { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding),
